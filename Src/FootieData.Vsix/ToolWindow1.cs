eb333e8364452ae62be2de898f08bf30353034b6
Greg Trevellick <gtrevellick@googlemail.com>
2018-01-06 23:42:06 +0000

    .

--- Src/FootieData.Vsix/ToolWindow1.cs
+++ Src/FootieData.Vsix/ToolWindow1.cs
@@ -23,12 +23,12 @@
         /// </summary>
         public ToolWindow1() : base(null)
         {
-            this.Caption = "Gregt League Tables";
+            Caption = "Gregt League Tables";
 
             // This is the user control hosted by the tool window; Note that, even if this class implements IDisposable,
             // we are not calling Dispose on this object. This is because ToolWindowPane calls Dispose on
             // the object returned by the Content property.
-            this.Content = new ToolWindow1Control();
+            Content = new ToolWindow1Control();
         }
     }
 }
