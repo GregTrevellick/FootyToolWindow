3a7445888d3138cef5d2a352e1264ed6509b5fd3
Greg Trevellick <gtrevellick@googlemail.com>
2018-01-09 21:32:39 +0000

    .

--- Src/zNew folder/MainWindow.cs
+++ Src/zNew folder/MainWindow.cs
@@ -15,40 +15,40 @@ namespace HierarchicalDataTemplate
 
         private void DataGridLoaded_PLs(object sender, RoutedEventArgs e)
         {
-            var leagueResponse = GetLeagueResponse();
             var grid = sender as DataGrid;
+            var leagueResponse = GetLeagueResponse(grid.Name);
             grid.ItemsSource = leagueResponse.Standings;
             grid.GridLinesVisibility = DataGridGridLinesVisibility.None;
         }
 
         private void DataGridLoaded_PLr(object sender, RoutedEventArgs e)
         {
-            var leagueResponse = GetLeagueResponse();
             var grid = sender as DataGrid;
+            var leagueResponse = GetLeagueResponse(grid.Name);
             grid.ItemsSource = leagueResponse.Results;
             grid.GridLinesVisibility = DataGridGridLinesVisibility.None;
         }
 
         private void DataGridLoaded_PLf(object sender, RoutedEventArgs e)
         {
-            var leagueResponse = GetLeagueResponse();
             var grid = sender as DataGrid;
+            var leagueResponse = GetLeagueResponse(grid.Name);
             grid.ItemsSource = leagueResponse.Fixtures;
             grid.GridLinesVisibility = DataGridGridLinesVisibility.None;
         }
 
         private void DataGridLoaded_BL1s(object sender, RoutedEventArgs e)
         {
-            var leagueResponse = GetLeagueResponse();
             var grid = sender as DataGrid;
+            var leagueResponse = GetLeagueResponse(grid.Name);
             grid.ItemsSource = leagueResponse.Standings;
             grid.GridLinesVisibility = DataGridGridLinesVisibility.None;
         }
 
         private void DataGridLoaded_BL2s(object sender, RoutedEventArgs e)
         {
-            var leagueResponse = GetLeagueResponse();
             var grid = sender as DataGrid;
+            var leagueResponse = GetLeagueResponse(grid.Name);
             grid.ItemsSource = leagueResponse.Standings;
             grid.GridLinesVisibility = DataGridGridLinesVisibility.None;
         }
@@ -70,26 +70,62 @@ namespace HierarchicalDataTemplate
         //    grid.GridLinesVisibility = DataGridGridLinesVisibility.None;
         //}
 
-        private LeagueResponse GetLeagueResponse()
+        private LeagueResponse GetLeagueResponse(string gridName)
         {
-            LeagueResponse league2 = new LeagueResponse("Serie a")
+            LeagueResponse league2 = new LeagueResponse();
+
+            switch (gridName)
             {
-                Standings = new List<Standing> {
-                    new Standing() {Team = "roma", Rank = 1, Points = 30},
-                    new Standing() {Team = "inter", Rank = 2, Points = 20},
-                    new Standing() {Team = "naples", Rank = 3, Points = 15},
-                },
-                Results = new List<Fixture>
-                {
-                    new Fixture{AwayTeamName="naples", HomeTeamName="roma", Date = new DateTime(2017,02,03),goalsAwayTeam=3,goalsHomeTeam=3},
-                    new Fixture{AwayTeamName="inter", HomeTeamName="naples", Date = new DateTime(2017,02,03),goalsAwayTeam=2,goalsHomeTeam=2},
-                },
-                Fixtures = new List<Fixture>
-                {
-                    new Fixture{AwayTeamName="inter", HomeTeamName="naples", Date = new DateTime(2017,02,04),goalsHomeTeam=1},
-                    new Fixture{AwayTeamName="naples", HomeTeamName="roma", Date = new DateTime(2017,02,05),goalsAwayTeam=5,goalsHomeTeam=5},
-                },
-            };
+                case "PLs":
+                case "PLr":
+                case "PLf":
+                    league2 = new LeagueResponse("Serie a")
+                    {
+                        Standings = new List<Standing> {
+                            new Standing() {Team = "mancity", Rank = 1, Points = 30},
+                            new Standing() {Team = "chelsea", Rank = 2, Points = 20},
+                            new Standing() {Team = "manutd", Rank = 3, Points = 15},
+                        },
+                        Results = new List<Fixture>
+                        {
+                            new Fixture{AwayTeamName="united", HomeTeamName="city", Date = new DateTime(2017,02,03),goalsAwayTeam=3,goalsHomeTeam=3},
+                            new Fixture{AwayTeamName="united", HomeTeamName="city", Date = new DateTime(2017,02,03),goalsAwayTeam=2,goalsHomeTeam=2},
+                        },
+                        Fixtures = new List<Fixture>
+                        {
+                            new Fixture{AwayTeamName="everton", HomeTeamName="everton", Date = new DateTime(2017,02,04)},
+                            new Fixture{AwayTeamName="west ham", HomeTeamName="saints", Date = new DateTime(2017,02,05)},
+                            new Fixture{AwayTeamName="everton", HomeTeamName="everton", Date = new DateTime(2017,02,04)},
+                            new Fixture{AwayTeamName="west ham", HomeTeamName="saints", Date = new DateTime(2017,02,05)},
+                        },
+                    };
+                    break;
+                case "BL2s":
+                    league2 = new LeagueResponse("Serie a")
+                    {
+                        Standings = new List<Standing> {
+                            new Standing() {Team = "roma", Rank = 1, Points = 30},
+                            new Standing() {Team = "inter", Rank = 2, Points = 20},
+                            new Standing() {Team = "naples", Rank = 3, Points = 15},
+                            new Standing() {Team = "naples", Rank = 3, Points = 15},
+                            new Standing() {Team = "naples", Rank = 3, Points = 15},
+                            new Standing() {Team = "naples", Rank = 3, Points = 15},
+                        },
+                        Results = new List<Fixture>
+                        {
+                            new Fixture{AwayTeamName="naples", HomeTeamName="roma", Date = new DateTime(2017,02,03),goalsAwayTeam=3,goalsHomeTeam=3},
+                            new Fixture{AwayTeamName="inter", HomeTeamName="naples", Date = new DateTime(2017,02,03),goalsAwayTeam=2,goalsHomeTeam=2},
+                        },
+                        Fixtures = new List<Fixture>
+                        {
+                            new Fixture{AwayTeamName="inter", HomeTeamName="naples", Date = new DateTime(2017,02,04),goalsHomeTeam=1},
+                            new Fixture{AwayTeamName="naples", HomeTeamName="roma", Date = new DateTime(2017,02,05),goalsAwayTeam=5,goalsHomeTeam=5},
+                        },
+                    };
+                    break;
+            }
+
+            
             return league2;
         }
         
