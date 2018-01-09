5122d70fe7c1ba4c188da164c0d3071581dc390e
Greg Trevellick <GregTrevellick@users.noreply.github.com>
2018-01-09 08:47:49 +0000

    Create GeneralOptions.cs

---
+++ Src/FootieData.Vsix/GeneralOptions.cs
@@ -0,0 +1,39 @@
+using Microsoft.VisualStudio.Shell;
+using SolutionOpenPopUp.Helpers;
+using System.ComponentModel;
+
+namespace FootieData.Vsix.Options
+{
+    public class GeneralOptions : DialogPage
+    {
+        [Category(CommonConstants.CategorySubLevel)]
+        [DisplayName("Show premier league")]
+        [Description("todo")]
+        public bool ShowPL { get; set; } = true;
+
+        [Category(CommonConstants.CategorySubLevel)]
+        [DisplayName("Expand premier league")]
+        [Description("todo")]
+        public bool ExpandPL { get; set; } = false;
+
+        [Category(CommonConstants.CategorySubLevel)]
+        [DisplayName("Show budesliga 1")]
+        [Description("todo")]
+        public bool ShowBL1 { get; set; } = true;
+
+        [Category(CommonConstants.CategorySubLevel)]
+        [DisplayName("Expand budesliga 1")]
+        [Description("todo")]
+        public bool ExpandL1 { get; set; } = false;
+
+        [Category(CommonConstants.CategorySubLevel)]
+        [DisplayName("Show budesliga 2")]
+        [Description("todo")]
+        public bool ShowBL2 { get; set; } = false;
+        
+        [Category(CommonConstants.CategorySubLevel)]
+        [DisplayName("Expand budesliga 2")]
+        [Description("todo")]
+        public bool ExpandBL2 { get; set; } = false;
+    }
+}
