ac74a039a3d6e72b083e45f469a893b334b57449
Greg Trevellick <gtrevellick@googlemail.com>
2018-01-14 01:02:21 +0000

    .

--- Src/zNew folder/MainWindow.cs
+++ Src/zNew folder/MainWindow.cs
@@ -20,46 +20,46 @@ namespace HierarchicalDataTemplate
                     ShowLeague = true,
                     LeagueSubOptions = new List<LeagueSubOption>
                     {new LeagueSubOption{Expand = true,GridType = GridType.Standing}}},
-                new LeagueOption{
-                    InternalLeagueCode= HierarchicalDataTemplate.InternalLeagueCode.UK1,
-                    ShowLeague = true,
-                    LeagueSubOptions = new List<LeagueSubOption>
-                    {new LeagueSubOption{Expand = true,GridType = GridType.Result}}},
-                new LeagueOption{
-                    InternalLeagueCode= HierarchicalDataTemplate.InternalLeagueCode.UK1,
-                    ShowLeague = true,
-                    LeagueSubOptions = new List<LeagueSubOption>
-                    {new LeagueSubOption {Expand =false,GridType = GridType.Fixture}}},
-                new LeagueOption{
-                    InternalLeagueCode= HierarchicalDataTemplate.InternalLeagueCode.DE1,
-                    ShowLeague = true,
-                    LeagueSubOptions = new List<LeagueSubOption>
-                        {new LeagueSubOption{Expand = true,GridType = GridType.Standing}}},
-                new LeagueOption{
-                    InternalLeagueCode= HierarchicalDataTemplate.InternalLeagueCode.DE1,
-                    ShowLeague = true,
-                    LeagueSubOptions = new List<LeagueSubOption>
-                        {new LeagueSubOption{Expand = true,GridType = GridType.Result}}},
-                new LeagueOption{
-                    InternalLeagueCode= HierarchicalDataTemplate.InternalLeagueCode.DE1,
-                    ShowLeague = true,
-                    LeagueSubOptions = new List<LeagueSubOption>
-                        {new LeagueSubOption {Expand = false,GridType = GridType.Fixture}}},
-                new LeagueOption{
-                    InternalLeagueCode= HierarchicalDataTemplate.InternalLeagueCode.DE2,
-                    ShowLeague = false},
-                    //LeagueSubOptions = new List<LeagueSubOption>
-                    //    {new LeagueSubOption{Expand = true,GridType = GridType.Standing}}},
-                new LeagueOption{
-                    InternalLeagueCode= HierarchicalDataTemplate.InternalLeagueCode.DE2,
-                    ShowLeague = false},
-                    //LeagueSubOptions = new List<LeagueSubOption>
-                    //    {new LeagueSubOption{Expand = true,GridType = GridType.Result}}},
-                new LeagueOption{
-                    InternalLeagueCode= HierarchicalDataTemplate.InternalLeagueCode.DE2,
-                    ShowLeague = false},
-                    //LeagueSubOptions = new List<LeagueSubOption>
-                    //    {new LeagueSubOption {Expand = false,GridType = GridType.Fixture}}},
+                //new LeagueOption{
+                //    InternalLeagueCode= HierarchicalDataTemplate.InternalLeagueCode.UK1,
+                //    ShowLeague = true,
+                //    LeagueSubOptions = new List<LeagueSubOption>
+                //    {new LeagueSubOption{Expand = true,GridType = GridType.Result}}},
+                //new LeagueOption{
+                //    InternalLeagueCode= HierarchicalDataTemplate.InternalLeagueCode.UK1,
+                //    ShowLeague = true,
+                //    LeagueSubOptions = new List<LeagueSubOption>
+                //    {new LeagueSubOption {Expand =false,GridType = GridType.Fixture}}},
+                //new LeagueOption{
+                //    InternalLeagueCode= HierarchicalDataTemplate.InternalLeagueCode.DE1,
+                //    ShowLeague = true,
+                //    LeagueSubOptions = new List<LeagueSubOption>
+                //        {new LeagueSubOption{Expand = true,GridType = GridType.Standing}}},
+                //new LeagueOption{
+                //    InternalLeagueCode= HierarchicalDataTemplate.InternalLeagueCode.DE1,
+                //    ShowLeague = true,
+                //    LeagueSubOptions = new List<LeagueSubOption>
+                //        {new LeagueSubOption{Expand = true,GridType = GridType.Result}}},
+                //new LeagueOption{
+                //    InternalLeagueCode= HierarchicalDataTemplate.InternalLeagueCode.DE1,
+                //    ShowLeague = true,
+                //    LeagueSubOptions = new List<LeagueSubOption>
+                //        {new LeagueSubOption {Expand = false,GridType = GridType.Fixture}}},
+                //new LeagueOption{
+                //    InternalLeagueCode= HierarchicalDataTemplate.InternalLeagueCode.DE2,
+                //    ShowLeague = false},
+                //    //LeagueSubOptions = new List<LeagueSubOption>
+                //    //    {new LeagueSubOption{Expand = true,GridType = GridType.Standing}}},
+                //new LeagueOption{
+                //    InternalLeagueCode= HierarchicalDataTemplate.InternalLeagueCode.DE2,
+                //    ShowLeague = false},
+                //    //LeagueSubOptions = new List<LeagueSubOption>
+                //    //    {new LeagueSubOption{Expand = true,GridType = GridType.Result}}},
+                //new LeagueOption{
+                //    InternalLeagueCode= HierarchicalDataTemplate.InternalLeagueCode.DE2,
+                //    ShowLeague = false},
+                //    //LeagueSubOptions = new List<LeagueSubOption>
+                //    //    {new LeagueSubOption {Expand = false,GridType = GridType.Fixture}}},
             }
         };
 
