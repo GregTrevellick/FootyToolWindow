c921e17f27b6cfb37a6db8d2660f6d96d4e6bdc4
Greg Trevellick <gtrevellick@googlemail.com>
2018-01-14 01:21:11 +0000

    .

--- Src/zNew folder/MainWindow.cs
+++ Src/zNew folder/MainWindow.cs
@@ -20,11 +20,11 @@ namespace HierarchicalDataTemplate
                     ShowLeague = true,
                     LeagueSubOptions = new List<LeagueSubOption>
                     {new LeagueSubOption{Expand = true,GridType = GridType.Standing}}},
-                //new LeagueOption{
-                //    InternalLeagueCode= HierarchicalDataTemplate.InternalLeagueCode.UK1,
-                //    ShowLeague = true,
-                //    LeagueSubOptions = new List<LeagueSubOption>
-                //    {new LeagueSubOption{Expand = true,GridType = GridType.Result}}},
+                new LeagueOption{
+                    InternalLeagueCode= HierarchicalDataTemplate.InternalLeagueCode.UK1,
+                    ShowLeague = true,
+                    LeagueSubOptions = new List<LeagueSubOption>
+                    {new LeagueSubOption{Expand = true,GridType = GridType.Result}}},
                 //new LeagueOption{
                 //    InternalLeagueCode= HierarchicalDataTemplate.InternalLeagueCode.UK1,
                 //    ShowLeague = true,
