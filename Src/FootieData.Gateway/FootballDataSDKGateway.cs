d569a049f5289e7c3f9ad9ae05ee6ccdd7362023
Greg Trevellick <gtrevellick@googlemail.com>
2018-01-20 23:09:16 +0000

    .

--- Src/FootieData.Gateway/FootballDataSDKGateway.cs
+++ Src/FootieData.Gateway/FootballDataSDKGateway.cs
@@ -12,6 +12,12 @@ namespace FootieData.Gateway
 {
     public class FootballDataSdkGateway
     {
+        //gregt for testing
+        private static CultureInfo enUS = new CultureInfo("en-US");
+        private static CultureInfo enGB = new CultureInfo("en-GB");
+        private static CultureInfo frFR = new CultureInfo("fr-FR");
+        private static CultureInfo deDE = new CultureInfo("de-DE");
+
         private readonly FootDataServices _footDataServices;
         private readonly SoccerSeasonResultSingleton _soccerSeasonResultSingleton;
 
@@ -114,16 +120,10 @@ namespace FootieData.Gateway
             }
             else
             {
-                //gregt for testing
-                //var ci = new CultureInfo("en-US");
-                //var ci = new CultureInfo("en-GB");
-                //var ci = new CultureInfo("fr-FR");
-                var ci = new CultureInfo("de-DE");
-
                 return fixturesResult?.fixtures?.Select(x => new FixturePast
                 {
                     AwayName = x.awayTeamName,
-                    Date = x.date.ToString("d", ci),
+                    Date = x.date.ToString("d", enGB),//gregt unit test & remove culture
                     HomeName = x.homeTeamName,
                     GoalsAway = x.result?.goalsAwayTeam,
                     GoalsHome = x.result?.goalsHomeTeam,
@@ -145,18 +145,12 @@ namespace FootieData.Gateway
             }
             else
             {
-                //gregt for testing
-                //var ci = new CultureInfo("en-US");
-                //var ci = new CultureInfo("en-GB");
-                //var ci = new CultureInfo("fr-FR");
-                var ci = new CultureInfo("de-DE");
-
                 return fixturesResult?.fixtures?.Select(x => new FixtureFuture
                 {
                     AwayName = x.awayTeamName,
-                    Date = x.date.ToString("d", ci),
+                    Date = x.date.ToString("d", enGB),//gregt unit test & remove culture
                     HomeName = x.homeTeamName,
-                    Time = x.date.ToString("t", ci),
+                    Time = x.date.ToString("t", enUS),//gregt unit test & remove culture - to be tested
                 });
             }
         }
@@ -164,8 +158,10 @@ namespace FootieData.Gateway
 }
 
 
-
-
+//https://docs.microsoft.com/en-us/dotnet/standard/base-types/standard-date-and-time-format-strings
+//2009-06-15T13:45:30 -> 1:45 PM(en-US)
+//2009-06-15T13:45:30 -> 13:45 (hr-HR)
+//2009-06-15T13:45:30 -> 01:45 م(ar-EG)
 
 
 
