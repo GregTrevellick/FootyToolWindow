2013df63c98d275322e0853e45551b2e6aafb017
Greg Trevellick <gtrevellick@googlemail.com>
2018-01-21 08:26:18 +0000

    .

--- Src/zNew folder/MainWindow.cs
+++ Src/zNew folder/MainWindow.cs
@@ -22,6 +22,7 @@ namespace HierarchicalDataTemplate
         private static WpfHelper _wpfHelper;
         private static GeneralOptions _generalOptions;
         private readonly SolidColorBrush _color;
+        private readonly SolidColorBrush _colorRefreshed;
         private readonly SoccerSeasonResultSingleton _soccerSeasonResultSingletonInstance;
         private readonly IEnumerable<NullReturn> _nullStandings = new List<NullReturn> { new NullReturn { Error = $"League table {Unavailable}" } };
         private readonly IEnumerable<NullReturn> _nullFixturePasts = new List<NullReturn> { new NullReturn { Error = $"Results {Unavailable}" } };
@@ -33,9 +34,15 @@ namespace HierarchicalDataTemplate
             InitializeComponent();
 
             _color = new SolidColorBrush((Color) ColorConverter.ConvertFromString("#FFFFF0"));
+            _colorRefreshed = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#00FFFF"));
             _soccerSeasonResultSingletonInstance = SoccerSeasonResultSingleton.Instance;
             _wpfHelper = new WpfHelper();
 
+            GetOptions();
+        }
+
+        private static void GetOptions()
+        {
             _generalOptions = new TempryGetOptions().GetGeneralOptions();
         }
 
@@ -102,7 +109,7 @@ namespace HierarchicalDataTemplate
                         {
                             case GridType.Standing:
                                 var standings = await GetStandingsAsync(gridType, shouldShowLeague, internalLeagueCode); //wont run til web service call has finished
-                               dataGrid.ItemsSource = standings ?? (IEnumerable)_nullStandings;
+                                dataGrid.ItemsSource = standings ?? (IEnumerable)_nullStandings;
                                 break;
                             case GridType.Result:
                                 var results = await GetFixturePastsAsync(gridType, shouldShowLeague, internalLeagueCode); //wont run til web service call finished
@@ -252,6 +259,75 @@ namespace HierarchicalDataTemplate
             }
 
             return Application.Current.Resources[resourceKey];
+        }
+
+        private void Click_HandlerRefresh(object sender, RoutedEventArgs e)
+        {
+            GetOptions();
+
+            var dataGrid = FindChild<DataGrid>(Application.Current.MainWindow, "Standing_Rand0001");
+            DataGridLoaded_Any(dataGrid, null);
+            dataGrid.AlternatingRowBackground = _colorRefreshed;
+
+            dataGrid = FindChild<DataGrid>(Application.Current.MainWindow, "Results1_Rand0001");
+            DataGridLoaded_Any(dataGrid, null);
+            dataGrid.AlternatingRowBackground = _colorRefreshed;
+
+            dataGrid = FindChild<DataGrid>(Application.Current.MainWindow, "Fixtures_Rand0001");
+            DataGridLoaded_Any(dataGrid, null);
+            dataGrid.AlternatingRowBackground = _colorRefreshed;
+        }
+
+        /// <summary>
+        /// Finds a Child of a given item in the visual tree. 
+        /// </summary>
+        /// <param name="parent">A direct parent of the queried item.</param>
+        /// <typeparam name="T">The type of the queried item.</typeparam>
+        /// <param name="childName">x:Name or Name of child. </param>
+        /// <returns>The first parent item that matches the submitted type parameter. 
+        /// If not matching item can be found, 
+        /// a null parent is being returned.</returns>
+        public static T FindChild<T>(DependencyObject parent, string childName) where T : DependencyObject
+        {
+            // Confirm parent and childName are valid. 
+            if (parent == null) return null;
+
+            T foundChild = null;
+
+            int childrenCount = VisualTreeHelper.GetChildrenCount(parent);
+            for (int i = 0; i < childrenCount; i++)
+            {
+                var child = VisualTreeHelper.GetChild(parent, i);
+                // If the child is not of the request child type child
+                T childType = child as T;
+                if (childType == null)
+                {
+                    // recursively drill down the tree
+                    foundChild = FindChild<T>(child, childName);
+
+                    // If the child is found, break so we do not overwrite the found child. 
+                    if (foundChild != null) break;
+                }
+                else if (!string.IsNullOrEmpty(childName))
+                {
+                    var frameworkElement = child as FrameworkElement;
+                    // If the child's name is set for search
+                    if (frameworkElement != null && frameworkElement.Name == childName)
+                    {
+                        // if the child's name is of the request name
+                        foundChild = (T)child;
+                        break;
+                    }
+                }
+                else
+                {
+                    // child element found.
+                    foundChild = (T)child;
+                    break;
+                }
+            }
+
+            return foundChild;
         }
     }
 }
