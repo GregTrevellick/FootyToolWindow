bc63ef3e38156a70b88d8c3591fd3eb47c2b9bdd
Greg Trevellick <gtrevellick@googlemail.com>
2018-01-21 08:29:29 +0000

    .

--- Src/zNew folder/MainWindow.cs
+++ Src/zNew folder/MainWindow.cs
@@ -3,17 +3,15 @@ using FootieData.Common;
 using FootieData.Common.Helpers;
 using FootieData.Entities;
 using FootieData.Gateway;
+using HierarchicalDataTemplate.ReferenceData;
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Globalization;
 using System.Linq;
-using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
-using HierarchicalDataTemplate.ReferenceData;
 
 namespace HierarchicalDataTemplate
 {
@@ -290,16 +288,22 @@ namespace HierarchicalDataTemplate
         public static T FindChild<T>(DependencyObject parent, string childName) where T : DependencyObject
         {
             // Confirm parent and childName are valid. 
-            if (parent == null) return null;
+            if (parent == null)
+            {
+                return null;
+            }
 
             T foundChild = null;
 
-            int childrenCount = VisualTreeHelper.GetChildrenCount(parent);
-            for (int i = 0; i < childrenCount; i++)
+            var childrenCount = VisualTreeHelper.GetChildrenCount(parent);
+
+            for (var i = 0; i < childrenCount; i++)
             {
                 var child = VisualTreeHelper.GetChild(parent, i);
+                
                 // If the child is not of the request child type child
                 T childType = child as T;
+
                 if (childType == null)
                 {
                     // recursively drill down the tree
@@ -308,22 +312,26 @@ namespace HierarchicalDataTemplate
                     // If the child is found, break so we do not overwrite the found child. 
                     if (foundChild != null) break;
                 }
-                else if (!string.IsNullOrEmpty(childName))
+                else
                 {
-                    var frameworkElement = child as FrameworkElement;
-                    // If the child's name is set for search
-                    if (frameworkElement != null && frameworkElement.Name == childName)
+                    if (!string.IsNullOrEmpty(childName))
                     {
-                        // if the child's name is of the request name
+                        var frameworkElement = child as FrameworkElement;
+
+                        // If the child's name is set for search
+                        if (frameworkElement != null && frameworkElement.Name == childName)
+                        {
+                            // if the child's name is of the request name
+                            foundChild = (T)child;
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        // child element found.
                         foundChild = (T)child;
                         break;
                     }
-                }
-                else
-                {
-                    // child element found.
-                    foundChild = (T)child;
-                    break;
                 }
             }
 
