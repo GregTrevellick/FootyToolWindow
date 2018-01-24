648fb294765c9354a1180810b184242761af2893
Greg Trevellick <gtrevellick@googlemail.com>
2018-01-24 12:34:08 +0000

    move ref data

--- Src/zNew folder/ReferenceData/InternalLeagueCode.cs
+++ Src/zNew folder/ReferenceData/InternalLeagueCode.cs
@@ -1,53 +1,53 @@
-﻿using System.ComponentModel;
+﻿//using System.ComponentModel;
 
-namespace HierarchicalDataTemplate.ReferenceData
-{
-    public enum InternalLeagueCode
-    {
-        [Description("English Premiere League")]
-        UK1,
-        [Description("English League One")]
-        UK2,
-        [Description("English Championship")]
-        UK3,
-        [Description("English F.A. Cup")]
-        UK4,       
-        [Description("Bundesliga 1")]
-        DE1,
-        [Description("Bundesliga 2 ")]
-        DE2,
-        [Description("Bundesliga 3")]
-        DE3,
-        [Description("Dfb-Cup")]
-        DE4,
-        [Description("Serie A")]
-        IT1,
-        [Description("Serie B")]
-        IT2,
-        [Description("Primera Division")]
-        ES1,
-        [Description("Segunda Division")]
-        ES2,
-        [Description("Copa del Rey")]
-        ES3,
-        [Description("Ligue 1")]
-        FR1,
-        [Description("Ligue 2")]
-        FR2,
-        [Description("Eredivisie")]
-        NL1,
-        [Description("Primeira Liga")]
-        PT1,
-        [Description("Greece Super League")]
-        GR1,
-        [Description("UEFA Champions League")]
-        UEFA1,
-        [Description("UEFA Europa Cup")]
-        UEFA2,
-        [Description("UEFA Europe European-Cup of Nations")]
-        UEFA3,
-        [Description("FIFA World Cup")]
-        FIFA1,
-    }
+//namespace HierarchicalDataTemplate.ReferenceData
+//{
+//    public enum InternalLeagueCode
+//    {
+//        [Description("English Premiere League")]
+//        UK1,
+//        [Description("English League One")]
+//        UK2,
+//        [Description("English Championship")]
+//        UK3,
+//        [Description("English F.A. Cup")]
+//        UK4,       
+//        [Description("Bundesliga 1")]
+//        DE1,
+//        [Description("Bundesliga 2 ")]
+//        DE2,
+//        [Description("Bundesliga 3")]
+//        DE3,
+//        [Description("Dfb-Cup")]
+//        DE4,
+//        [Description("Serie A")]
+//        IT1,
+//        [Description("Serie B")]
+//        IT2,
+//        [Description("Primera Division")]
+//        ES1,
+//        [Description("Segunda Division")]
+//        ES2,
+//        [Description("Copa del Rey")]
+//        ES3,
+//        [Description("Ligue 1")]
+//        FR1,
+//        [Description("Ligue 2")]
+//        FR2,
+//        [Description("Eredivisie")]
+//        NL1,
+//        [Description("Primeira Liga")]
+//        PT1,
+//        [Description("Greece Super League")]
+//        GR1,
+//        [Description("UEFA Champions League")]
+//        UEFA1,
+//        [Description("UEFA Europa Cup")]
+//        UEFA2,
+//        [Description("UEFA Europe European-Cup of Nations")]
+//        UEFA3,
+//        [Description("FIFA World Cup")]
+//        FIFA1,
+//    }
 
-}
+//}
