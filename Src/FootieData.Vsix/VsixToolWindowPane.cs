bdbbbcb1dfd4319e92b41f6939d992d9cda416b7
Greg Trevellick <gtrevellick@googlemail.com>
2018-02-15 20:03:31 +0000

    .

--- Src/FootieData.Vsix/VsixToolWindowPane.cs
+++ Src/FootieData.Vsix/VsixToolWindowPane.cs
@@ -4,9 +4,10 @@
     using System.Runtime.InteropServices;
     using Microsoft.VisualStudio.Shell;
 
-    /// This class implements the tool window exposed by this package and hosts a user control.
-    /// In Visual Studio tool windows are composed of a frame (implemented by the shell) and a pane, usually implemented by the package implementer.
-    /// This class derives from the ToolWindowPane class provided from the MPF in order to use its implementation of the IVsUIElementPane interface.
+    // This class implements the tool window exposed by this package and hosts a user control.
+    // In Visual Studio tool windows are composed of a frame (implemented by the shell) and a pane, usually implemented by the package implementer.
+    // This class derives from the ToolWindowPane class provided from the MPF in order to use its implementation of the IVsUIElementPane interface.
+
     [Guid("c53b01a9-7130-4b7f-957d-cdc8672fa6de")]
     public class VsixToolWindowPane : ToolWindowPane
     {
@@ -16,8 +17,8 @@
         {
             Caption = "League Tables";
 
-            // This is the user control hosted by the tool window; Note that, even if this class implements IDisposable,
-            // we are not calling Dispose on this object. This is because ToolWindowPane calls Dispose on the object returned by the Content property.
+            // This is the user control hosted by the tool window; Note that, even if this class implements IDisposable, we are not calling Dispose on this object. 
+            // This is because ToolWindowPane calls Dispose on the object returned by the Content property.
             Content = new ToolWindow1Control(GetOptionsFromStoreAndMapToInternalFormatMethod);
         }
     }
