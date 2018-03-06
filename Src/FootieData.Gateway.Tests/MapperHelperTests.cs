63e84612793fd17fd4d0a22b88b5479092ee8795
Greg Trevellick <gtrevellick@googlemail.com>
2018-03-06 21:03:37 +0000

    .

---
+++ Src/FootieData.Gateway.Tests/MapperHelperTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FootieData.Gateway.Tests
+{
+    [TestClass]
+    public class MapperHelperTests
+    {
+        [TestMethod]
+        public void MapExternalTeamNameToInternalTeamNameTest()
+        {
+            Assert.AreEqual(null, MapperHelper.MapExternalTeamNameToInternalTeamName(null));
+            Assert.AreEqual(string.Empty, MapperHelper.MapExternalTeamNameToInternalTeamName(""));
+            Assert.AreEqual("Manchester United", MapperHelper.MapExternalTeamNameToInternalTeamName("ManU"));
+            Assert.AreEqual("Arsenal", MapperHelper.MapExternalTeamNameToInternalTeamName("Arsenal"));
+        }
+    }
+}
