22ead8618f408f9037bb744b185adc4f50694753
Greg Trevellick <gtrevellick@googlemail.com>
2018-04-19 08:04:58 +0100

    .

--- Src/FootieData.Vsix/MyDataGrid.cs
+++ Src/FootieData.Vsix/MyDataGrid.cs
@@ -45,29 +45,59 @@ namespace FootieData.Vsix
 
             if (standing.Team.Contains("Chelsea"))//gregt replace .Team with .NewErrorText
             {
-                
-                // HIDE IT !
-
-                var _hideHeaderStyle = new Style();
-                var color = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#a75ced"));
-                _hideHeaderStyle.Setters.Add(new Setter(BackgroundProperty, color));
-                //_hideHeaderStyle.Setters.Add(new Setter(HeadersVisibilityProperty, DataGridHeadersVisibility.None));//"None"));
-
-                var _hideCellStyle = new Style();
-                color = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#728769"));
-                _hideCellStyle.Setters.Add(new Setter(BackgroundProperty, color));//gregt replace green background with hiding of the columns (except for the NewErrorText column)
-                //_hideCellStyle.Setters.Add(new Setter(MaxWidthProperty, (double)0));
-                //_hideCellStyle.Setters.Add(new Setter(ColumnWidthProperty, (double)0)); 
-                
-                ////////////////DataGridRow row = e.Row as DataGridRow;
-                ////////////////row.Visibility == Visibility.Collapsed;
-
                 foreach (var item in this.Columns)
                 {
-                    item.HeaderStyle = _hideHeaderStyle;
-                    item.CellStyle = _hideCellStyle;
+                    if (item.SortMemberPath == "Team")//gregt replace .Team with .NewErrorText
+                    {
+                        item.Visibility = Visibility.Hidden;
+                    }
                 }
             }
         }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//////////////////////////////////////////_hideCellStyle.Setters.Add(new Setter(MaxWidthProperty, (double)0));
+//////////////////////////////////////////_hideCellStyle.Setters.Add(new Setter(ColumnWidthProperty, (double)0)); 
+//////////////////////////////////////////_hideCellStyle.Setters.Add(new Setter(VisibilityProperty, Visibility.Collapsed));
+
+//////////////////////////////////////////var CellStyle = new Style(typeof(DataGridCell));
+////////////////////////////////////////////CellStyle.Setters.Add(new Setter(DataGridCell.MaxWidthProperty, 0));
+//////////////////////////////////////////CellStyle.Setters.Add(new Setter(DataGridCell.VisibilityProperty, Visibility.Collapsed));
+////////////////////////////////////////////_hideCellStyle.Setters.Add(new Setter(CellStyleProperty, Visibility.Collapsed));
+//////////////////////////////////////////_hideCellStyle = CellStyle;
+
+
+//////////////////////////////////////////https://stackoverflow.com/questions/39725214/datagrid-cell-styling-with-dynamic-generating-column?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
+
+//////////////////////////////////////////DataGridRow row = e.Row as DataGridRow;
+//////////////////////////////////////////row.Visibility == Visibility.Collapsed;
+
+////////////////////////////////////////item.CellStyle = _hideCellStyle;
+
+
+//var _hideHeaderStyle = new Style();
+//var color = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#a75ced"));
+//_hideHeaderStyle.Setters.Add(new Setter(BackgroundProperty, color));
+//                //_hideHeaderStyle.Setters.Add(new Setter(HeadersVisibilityProperty, DataGridHeadersVisibility.None));//"None"));
+
+
+
+//var _hideCellStyle = new Style();
+//color = new SolidColorBrush((Color) ColorConverter.ConvertFromString("#728769"));
+//                _hideCellStyle.Setters.Add(new Setter(BackgroundProperty, color));//gregt replace green background with hiding of the columns (except for the NewErrorText column)
+/////item.HeaderStyle = _hideHeaderStyle;
